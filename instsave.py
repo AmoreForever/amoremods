6b212176be6caad3fcb0ddf7dc755843a33f74ee
Fazliddin <103936585+AmoreForever@users.noreply.github.com>
2022-08-17 17:44:04 +0500

    add meta pic(instasave)

--- instsave.py
+++ instsave.py
@@ -11,6 +11,7 @@
 # scope: hikka_only
 # scope: hikka_min 1.3.0
 # meta developer: @amoremods
+# meta pic: https://imgur.com/4W7CzvT
 # meta banner: https://imgur.com/fQebpqe
 
 from telethon import events
