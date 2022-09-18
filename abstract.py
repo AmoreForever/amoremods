83c0ce3db03b67685eb0ce7257b465ea11b4466c
Fazliddin <103936585+AmoreForever@users.noreply.github.com>
2022-09-18 11:16:58 +0500

    Update abstract.py

--- abstract.py
+++ abstract.py
@@ -13,7 +13,7 @@
 # scope: hikka_min 1.3.3
 # meta developer: @amoremods
 # meta pic: https://te.legra.ph/file/868a280910e7f61f6ab0e.png
-# meta banner: https://te.legra.ph/file/f793af68784694e7ac38c.jpg
+# meta banner: https://raw.githubusercontent.com/AmoreForever/assets/master/Abstract.jpg
 
 from telethon import events
 from .. import utils, loader
