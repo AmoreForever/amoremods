66cd154412fb84ef69d589ebffca20f421f49ae0
Fazliddin <103936585+AmoreForever@users.noreply.github.com>
2022-09-25 12:13:51 +0500

    Create facts.py

---
+++ facts.py
@@ -0,0 +1,46 @@
+#          ▄▀█ █▀▄▀█ █▀█ █▀█ █▀▀
+#          █▀█ █░▀░█ █▄█ █▀▄ ██▄
+#
+#            © Copyright 2022
+#
+#          https://t.me/the_farkhodov
+#
+# 🔒 Licensed under the GNU GPLv3
+# 🌐 https://www.gnu.org/licenses/agpl-3.0.html
+
+# meta developer: @amoremods
+# meta banner: https://raw.githubusercontent.com/AmoreForever/assets/master/Facts.jpg
+
+chat = "@facti_p"
+
+import datetime
+import random
+from asyncio import sleep
+from telethon import functions
+from .. import loader, utils
+
+class FactsMod(loader.Module):
+    """More Interesting Facts"""
+
+    strings = {
+        "name": "Facts",
+        "wait": "<emoji document_id=5472146462362048818>💡</emoji> Searching..."
+        }
+    
+    strings_ru = {
+        "name": "Facts",
+        "wait": "<emoji document_id=5472146462362048818>💡</emoji> Поиск..."
+        }
+
+    @loader.command(ru_docs="Интересные Факты")
+    async def afactscmd(self, message):
+        """Intersting Facts"""
+        await utils.answer(message, self.strings["wait"])
+        result = await message.client(
+            functions.messages.GetHistoryRequest(
+                peer=chat, offset_id=0, offset_date=datetime.datetime.now(), add_offset=random.randint(0, 1000), limit=1, max_id=0, min_id=0, hash=0,
+            )
+        )
+        await sleep(0.30)
+        await message.delete()
+        await message.client.send_message(message.to_id, result.messages[0])
