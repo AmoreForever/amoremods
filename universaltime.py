94552c89023c594578735708bfb1ce4a8420b8a0
Fazliddin <103936585+AmoreForever@users.noreply.github.com>
2022-10-05 05:09:55 +0500

    fixed by skillz

--- universaltime.py
+++ universaltime.py
@@ -10,6 +10,8 @@
 
 # meta developer: @amoremods
 # meta banner: https://raw.githubusercontent.com/AmoreForever/assets/master/Universaltime.jpg
+
+
 from .. import loader, utils
 import datetime
 from ..inline.types import InlineCall
@@ -19,83 +21,38 @@ import logging
 logger = logging.getLogger(__name__)
 
 def check_time():
-	
-	offset = datetime.timedelta(hours=3)
-	tz = datetime.timezone(offset)
-	time1 = datetime.datetime.now(tz)
-	ru = time1.strftime("%d.%m.%y | %H:%M")
-	offset = datetime.timedelta(hours=5)
-	tz = datetime.timezone(offset)
-	time1 = datetime.datetime.now(tz)
-	uz = time1.strftime("%d.%m.%y | %H:%M")
-	offset = datetime.timedelta(hours=4)
-	tz = datetime.timezone(offset)
-	time1 = datetime.datetime.now(tz)
-	az = time1.strftime("%d.%m.%y | %H:%M")
-	offset = datetime.timedelta(hours=2)
-	tz = datetime.timezone(offset)
-	time1 = datetime.datetime.now(tz)
-	gr = time1.strftime("%d.%m.%y | %H:%M")
-	offset = datetime.timedelta(hours=1)
-	tz = datetime.timezone(offset)
-	time1 = datetime.datetime.now(tz)
-	uk = time1.strftime("%d.%m.%y | %H:%M")
-	offset = datetime.timedelta(hours=-7)
-	tz = datetime.timezone(offset)
-	time1 = datetime.datetime.now(tz)
-	us = time1.strftime("%d.%m.%y | %H:%M")
-	offset = datetime.timedelta(hours=6)
-	tz = datetime.timezone(offset)
-	time1 = datetime.datetime.now(tz)
-	kgz = time1.strftime("%d.%m.%y | %H:%M")
-	offset = datetime.timedelta(hours=9)
-	tz = datetime.timezone(offset)
-	time1 = datetime.datetime.now(tz)
-	jp = time1.strftime("%d.%m.%y | %H:%M")
-	offset = datetime.timedelta(hours=5.30)
-	tz = datetime.timezone(offset)
-	time1 = datetime.datetime.now(tz)
-	ind = time1.strftime("%d.%m.%y | %H:%M")
-	offset = datetime.timedelta(hours=9)
-	tz = datetime.timezone(offset)
-	time1 = datetime.datetime.now(tz)
-	ch = time1.strftime("%d.%m.%y | %H:%M")
-	offset = datetime.timedelta(hours=8)
-	tz = datetime.timezone(offset)
-	time1 = datetime.datetime.now(tz)
-	mo = time1.strftime("%d.%m.%y | %H:%M")
-	offset = datetime.timedelta(hours=-8)
-	tz = datetime.timezone(offset)
-	time1 = datetime.datetime.now(tz)
-	ca = time1.strftime("%d.%m.%y | %H:%M")    
-	offset = datetime.timedelta(hours=-4)
-	tz = datetime.timezone(offset)
-	time1 = datetime.datetime.now(tz)
-	ar = time1.strftime("%d.%m.%y | %H:%M")  
+	offsets = [3, 5, 4, 2, 1, -7, 6, 9, 5.30, 9, 8, -8, -4]
+	hrs = []
+	for x in offsets:
+		offset = datetime.timedelta(hours=x)
+		not_tz = datetime.timezone(offset)
+		time = datetime.datetime.now(not_tz)
+		format_ = time.strftime("%d.%m.%y | %H:%M")
+		hrs.append(format_)
 	amore = (
 			f"<emoji document_id=4920662486778119009>🌐</emoji> <b>Universal time</b>\n\n" 
-			f"<emoji document_id=6323139226418284334>🇷🇺</emoji> Russia ➪ {ru}\n"
-			f"<emoji document_id=6323430017179059570>🇺🇿</emoji> Uzbekistan ➪ {uz}\n"
-			f"<emoji document_id=6323289850921354919>🇺🇦</emoji> Ukraine ➪ {ru}\n"
-			f"<emoji document_id=6323575251498174463>🇦🇿</emoji> Azerbaijan ➪ {az}\n"
-			f"<emoji document_id=6320817337033295141>🇩🇪</emoji> German ➪ {gr}\n"
-			f"<emoji document_id=6323589145717376403>🇬🇧</emoji> UK ➪ {uk}\n"
-			f"<emoji document_id=6323602387101550101>🇵🇱</emoji> Poland ➪ {gr}\n"
-			f"<emoji document_id=6323374027985389586>🇺🇸</emoji> USA ➪ {us}\n"
-			f"<emoji document_id=6323615997852910673>🇰🇬</emoji> Kyrgyzstan ➪ {kgz}\n"
-			f"<emoji document_id=6323135275048371614>🇰🇿</emoji> Kazakhstan ➪ {kgz}\n"
-			f"<emoji document_id=6323555846835930376>🇮🇶</emoji> Iraq ➪ {ru}\n"
-			f"<emoji document_id=6323356796576597627>🇯🇵</emoji> Japan ➪ {jp}\n"
-			f"<emoji document_id=6323152716910561397>🇰🇷</emoji> South KR ➪ {jp}\n"
-			f"<emoji document_id=6323181871148566277>🇮🇳</emoji> India ➪ {ind}\n"
-			f"<emoji document_id=6323570711717742330>🇫🇷</emoji> France ➪ {gr}\n"
-			f"<emoji document_id=6323453751168337485>🇨🇳</emoji> China ➪ {ch}\n"
-			f"<emoji document_id=6321003171678259486>🇹🇷</emoji> Turkey ➪ {ru}\n"
-            f"<emoji document_id=6323602322677040561>🇨🇱</emoji> Mongolia ➪ {mo}\n"
-            f"<emoji document_id=6323325327351219831>🇨🇦</emoji> Canada ➪ {ca}\n"
-            f"<emoji document_id=6323471399188957082>🇮🇹</emoji> Italia ➪ {uk}\n"
-            f"<emoji document_id=6323516260122363644>🇪🇬</emoji> Egypt ➪ {gr}\n"
-            f"<emoji document_id=6323236391463421376>🇦🇲</emoji> Armenia ➪ {ar}\n\n"
+			f"<emoji document_id=6323139226418284334>🇷🇺</emoji> Russia ➪ {hrs[0]}\n"
+			f"<emoji document_id=6323430017179059570>🇺🇿</emoji> Uzbekistan ➪ {hrs[1]}\n"
+			f"<emoji document_id=6323289850921354919>🇺🇦</emoji> Ukraine ➪ {hrs[0]}\n"
+			f"<emoji document_id=6323575251498174463>🇦🇿</emoji> Azerbaijan ➪ {hrs[2]}\n"
+			f"<emoji document_id=6320817337033295141>🇩🇪</emoji> German ➪ {hrs[3]}\n"
+			f"<emoji document_id=6323589145717376403>🇬🇧</emoji> UK ➪ {hrs[4]}\n"
+			f"<emoji document_id=6323602387101550101>🇵🇱</emoji> Poland ➪ {hrs[3]}\n"
+			f"<emoji document_id=6323374027985389586>🇺🇸</emoji> USA ➪ {hrs[5]}\n"
+			f"<emoji document_id=6323615997852910673>🇰🇬</emoji> Kyrgyzstan ➪ {hrs[6]}\n"
+			f"<emoji document_id=6323135275048371614>🇰🇿</emoji> Kazakhstan ➪ {hrs[6]}\n"
+			f"<emoji document_id=6323555846835930376>🇮🇶</emoji> Iraq ➪ {hrs[0]}\n"
+			f"<emoji document_id=6323356796576597627>🇯🇵</emoji> Japan ➪ {hrs[7]}\n"
+			f"<emoji document_id=6323152716910561397>🇰🇷</emoji> South KR ➪ {hrs[7]}\n"
+			f"<emoji document_id=6323181871148566277>🇮🇳</emoji> India ➪ {hrs[8]}\n"
+			f"<emoji document_id=6323570711717742330>🇫🇷</emoji> France ➪ {hrs[3]}\n"
+			f"<emoji document_id=6323453751168337485>🇨🇳</emoji> China ➪ {hrs[9]}\n"
+			f"<emoji document_id=6321003171678259486>🇹🇷</emoji> Turkey ➪ {hrs[0]}\n"
+            f"<emoji document_id=6323602322677040561>🇨🇱</emoji> Mongolia ➪ {hrs[10]}\n"
+            f"<emoji document_id=6323325327351219831>🇨🇦</emoji> Canada ➪ {hrs[11]}\n"
+            f"<emoji document_id=6323471399188957082>🇮🇹</emoji> Italia ➪ {hrs[2]}\n"
+            f"<emoji document_id=6323516260122363644>🇪🇬</emoji> Egypt ➪ {hrs[3]}\n"
+            f"<emoji document_id=6323236391463421376>🇦🇲</emoji> Armenia ➪ {hrs[12]}\n\n"
             f"<emoji document_id=5188216117272780281>🍙</emoji> #whyamore"
 	)
 	return amore
