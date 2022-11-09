a8a45907b60db35e233e8fb5e4d96f8eb5eb751f
Fazliddin <103936585+AmoreForever@users.noreply.github.com>
2022-11-09 15:17:31 +0500

    new module for newyear

---
+++ nytimer.py
@@ -0,0 +1,32 @@
+#          ▄▀█ █▀▄▀█ █▀█ █▀█ █▀▀
+#          █▀█ █░▀░█ █▄█ █▀▄ ██▄
+#
+#             © Copyright 2022
+#
+#          https://t.me/amorescam
+#
+# 🔒 Licensed under the GNU GPLv3
+# 🌐 https://www.gnu.org/licenses/agpl-3.0.html
+# meta developer: @amoremods
+# meta banner: https://raw.githubusercontent.com/AmoreForever/assets/master/Nytimer.jpg
+
+from .. import loader, utils
+import datetime
+from time import strftime
+
+
+@loader.tds
+class NYMod(loader.Module):
+    """Check how much is left until the new year"""
+
+    strings = {'name': 'NewYearTimer'}
+
+    async def nycmd(self, message):
+        """Check date"""
+        now = datetime.datetime.today()
+        ng = datetime.datetime(int(strftime('%Y')) + 1, 1, 1)
+        d = ng - now
+        mm, ss = divmod(d.seconds, 60)
+        hh, mm = divmod(mm, 60)
+        soon = '<b><emoji document_id=6334530007968253960>☃️</emoji> Until the <u>New Year</u>: {} d. {} h. {} m. {} s.</b>\n<b><emoji document_id=5393226077520798225>🥰</emoji> Wait for the new year together <u>Family</u></b>'.format(d.days, hh, mm, ss)
+        await utils.answer(message, soon)
