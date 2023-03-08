f1c63ae6e0abf09ee2768d8b2ab3d1756f0d87df
Amore <me.thefarkhodov@gmail.com>
2023-03-09 00:46:51 +0500

    new module poststealer

---
+++ poststealer.py
@@ -0,0 +1,104 @@
+__version__ = (1, 0, 0)
+
+# ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+# ⠿⠿⠿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠟⠛⠛⠛⠛⠛
+# ⣶⣦⣤⣤⣤⣤⣤⣤⣬⣭⣭⣍⣉⡙⠛⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⣋⣩⣭⣥⣤⣴⣶⣶⣶⣶⣶⣶⣶⣶⣶
+# ⣆⠀⠀⠀⢡⠁⠀⡀⠀⢸⠟⠻⣯⠙⠛⠷⣶⣬⡙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢉⣥⣶⡟⠻⣙⡉⠀⢰⡆⠀⠀⣡⠀⣧⠀⠀⠀⢨
+# ⠻⣦⠀⠀⠈⣇⣀⣧⣴⣿⣶⣶⣿⣷⠀⢀⡇⠉⠻⢶⣌⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣡⡶⠟⠉⠀⢣⠀⣿⠷⠀⠀⠀⠀⣿⡷⢀⠇⠀⠀⢠⣿
+# ⣦⡈⢧⡀⠀⠘⢮⡙⠛⠉⠀⠄⠙⢿⣀⠞⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀⠀⠈⠳⣄⠉⠓⠒⠚⠋⢀⡠⠋⠀⢀⣴⣏⣿
+# ⣿⣿⣿⣛⣦⣀⠀⠙⠓⠦⠤⣤⠔⠛⠁⠀⠀⠀⠀⠀⢀⣀⣹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣤⣤⣤⣤⣤⣀⣀⣀⣀⢙⢓⣒⡒⠚⠋⢠⣤⢶⣟⣽⣿⣿
+# ⣿⣿⣿⣿⣿⣿⣷⣦⠀⠀⣴⣿⣷⣶⣶⣶⣾⡖⢰⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣾⣿⣿⣶⣾⣿⣿⣿⣿⣿⣿
+# ⣿⣿⣿⣿⣿⣿⣿⣿⠀⢀⣿⣿⣿⣿⣿⣿⣿⠃⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+# ⣿⣿⣿⣿⣿⣿⣿⡏⠀⢸⣿⣿⣿⣿⣿⣿⣿⠁⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+# ⣿⣿⣿⣿⣿⣿⣿⣷⠀⢸⣿⣿⣿⣿⣿⣿⣿⠀⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+
+#                           🔒 Licensed under the GNU GPLv3
+#                    🌐 https://www.gnu.org/licenses/agpl-3.0.html
+#                            https://t.me/amorescam
+
+# meta developer: @amoremods
+# meta banner: https://raw.githubusercontent.com/AmoreForever/assets/master/poststeal.jpg
+
+
+from .. import loader, utils
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+@loader.tds
+class PostStealer(loader.Module):
+    "Steal post from another channel to your channel"
+
+    strings = {
+        'name': 'PostStealler',
+        'enable': '<b>Steal mode enabled.</b>',
+        'disable': '<b>Steal mode disabled.</b>',
+        'channel': 'channel id where ub will steal messages',
+        'my_channel': 'channel id where ub will send messages'
+    }
+    
+    strings_ru = {
+        'enable': '<b>StealMod включен.</b>',
+        'disable': '<b>StealMod отключен.</b>',
+        'channel': 'айди канала откуда юб будет пересылать сообщения',
+        'my_channel': 'айди канала куда юб будет пересылать сообщения'
+    }
+
+    def __init__(self):
+        self.config = loader.ModuleConfig(
+            loader.ConfigValue(
+                "my_channel",
+                None,
+                lambda: self.strings("my_channel"),
+            ),
+            loader.ConfigValue(
+                "channel",
+                None,
+                lambda: self.strings("channel"),
+            ),
+        )
+
+    async def client_ready(self, client, db):
+        self.client = client
+        self.db = db
+        
+    @loader.command()
+    async def smode(self, message):
+        """- off/on steal mode"""
+        
+        status = self.db.get(
+			"steal_status",
+			"status",
+		)
+        if status == "":
+            self.db.set("steal_status", "status", True)
+        if status == False:
+            self.db.set("steal_status", "status", True)
+            await utils.answer(message, self.strings("enable"))
+        else:
+            self.db.set("steal_status", "status", False)
+            await utils.answer(message, self.strings("disable"))
+   
+    async def watcher(self, message):
+        """Лень писать описание"""
+        status = self.db.get("steal_status", "status")
+        if status == False:
+            return False
+        if status == True:
+            steal = self.config['channel']
+            chatid = int(message.chat_id)
+            text = message.text
+            if chatid == steal:
+                if message.photo:
+                    await self._client.send_file(int(self.config['my_channel']), message.photo, caption=message.text if text else None, link_preview=False)
+                    logging.info(f'Stealed photo from {steal}')
+                if message.video:
+                    await self._client.send_file(int(self.config['my_channel']), message.video, caption=message.text if text else None, link_preview=False)
+                    logging.info(f'Stealed video from {steal}')
+                elif message.text:
+                    await message.client.send_message(int(self.config['my_channel']), message.text)
+                    logging.info(f'Stealed message from {steal}')
+            else:
+                return False
+
